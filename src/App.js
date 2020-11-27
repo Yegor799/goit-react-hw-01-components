90467c20a1a30ff8f04ec10d0c64217ba7171ba7
Yegor <egoryefim@gmail.com>
2020-11-27 17:34:54 +0200

    create first task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
+import Profile from './components/Profile/Profile'
+import user from './user.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Profile
+      imageUrl={user.avatar}
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      stats={user.stats}
+      // followers={user.stats.followers}
+      // views={user.stats.views}
+      // likes ={user.stats.likes}
+/>
+  
     </div>
-  );
+    );
 }
 
 export default App;
