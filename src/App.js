235dd7f4bddbbe99052206aabc2d5680eefeec9e
Yegor <egoryefim@gmail.com>
2020-12-01 10:53:24 +0200

    homework done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import Profile from './components/Profile/Profile'
-import user from './user.json'
+import Profile from './components/Profile/Profile';
+import user from './user.json';
+
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './statistical-data.json';
+
+import FriendList from './components/FriendList/FriendList';
+import friends from './friends.json';
+
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './transactions.json'
 
 function App() {
   return (
     <div>
-    <Profile
+
+      <div>
+        <h1>Profile</h1>
+      <Profile
       imageUrl={user.avatar}
       name={user.name}
       tag={user.tag}
       location={user.location}
       stats={user.stats}
-      // followers={user.stats.followers}
-      // views={user.stats.views}
-      // likes ={user.stats.likes}
-/>
-  
-    </div>
+        /> 
+      </div>
+      
+      <div>
+        <h1>Statistics</h1>
+      <Statistics stats = {statisticalData}/>
+      </div>
+
+      <div>
+        <h1>FriendList</h1>
+        <FriendList friends={friends }/>
+      </div>
+
+      <div>
+        <h1>Transaction History</h1>
+        <TransactionHistory items={transactions }/>
+      </div>
+        
+      
+      </div>
     );
 }
 
