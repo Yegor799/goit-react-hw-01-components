67649ee770702051b7bbdc46bd2f9a99f212b30f
Yegor <egoryefim@gmail.com>
2020-12-04 12:04:52 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Profile from './components/Profile/Profile';
-import user from './user.json';
+import user from './components/Profile/user.json';
 
 import Statistics from './components/Statistics/Statistics';
-import statisticalData from './statistical-data.json';
+import statisticalData from './components/Statistics/statistical-data.json';
 
 import FriendList from './components/FriendList/FriendList';
-import friends from './friends.json';
+import friends from './components/FriendList/friends.json';
 
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './transactions.json'
+import transactions from './components/TransactionHistory/transactions.json'
 
 function App() {
   return (
@@ -27,7 +27,8 @@ function App() {
       
       <div>
         <h1>Statistics</h1>
-      <Statistics stats = {statisticalData}/>
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
       </div>
 
       <div>
