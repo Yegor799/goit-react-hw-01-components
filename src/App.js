6f9977f029fa0bac5d80a2fb4b1614a2e24e50fa
Yegor <egoryefim@gmail.com>
2020-12-05 13:15:14 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Profile from './components/Profile/Profile';
-import user from './components/Profile/user.json';
+import user from './data/user.json';
 
 import Statistics from './components/Statistics/Statistics';
-import statisticalData from './components/Statistics/statistical-data.json';
+import statisticalData from './data/statistical-data.json';
 
 import FriendList from './components/FriendList/FriendList';
-import friends from './components/FriendList/friends.json';
+import friends from './data/friends.json';
 
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json'
+import transactions from './data/transactions.json'
 
 function App() {
   return (
